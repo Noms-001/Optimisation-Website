7652ab24a05d0569d44d96dda33eeecd4feb95d7
Noms-001 <randrichris17@gmail.com>
2026-03-31 02:32:35 +0300

    Create TypeArticle.php

---
+++ public/commons/models/TypeArticle.php
@@ -0,0 +1,16 @@
+<?php
+
+class TypeArticle
+{
+    public $id;
+    public $libelle;
+
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+}
